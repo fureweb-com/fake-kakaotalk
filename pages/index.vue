825e91f15e88da2311a819252acee09bdf13f054
fureweb-com <fureweb.com@gmail.com>
2018-10-23 15:20:35 +0900

    Initial Commit
    - Built default structure using vue client and nuxt-starter-template
    - Modify default nuxt.config.js file about build.extend method
    - Modify package.json's some scripts using pm2 node module
    - npm start, reload, logs launches app using pm2 node module

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="indexWrap">
+    <header>HEADER</header>
+    <section class="body">BODY</section>
+    <footer>FOOTER</footer>
+  </section>
+</template>
+
+<script>
+export default {
+  mounted() {
+
+  },
+}
+</script>

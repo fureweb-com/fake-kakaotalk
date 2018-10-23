aa28fac6dcff2b8fdcba3c3189123f0f2c6b3976
fureweb-com <fureweb.com@gmail.com>
2018-10-23 16:46:16 +0900

    Update index page
    - Use 'index' middleware for redirect to people page before rendering

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,5 @@
-<template>
-  <section class="indexWrap">
-    <header>HEADER</header>
-    <section class="body">BODY</section>
-    <footer>FOOTER</footer>
-  </section>
-</template>
-
 <script>
 export default {
-  mounted() {
-
-  },
+  middleware: 'index',
 }
 </script>

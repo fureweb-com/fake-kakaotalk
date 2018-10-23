6d26cf158dc4c3c73b6ff7e126e5e20d09b5b07e
fureweb-com <fureweb.com@gmail.com>
2018-10-23 15:48:17 +0900

    Add people.search._type page
    - path: /prople/search/:type
    - It can be used when want to find another user by id or phoneNumber.

---
+++ pages/people/search/_type.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="peopleSearchWrap">
+    type: {{type}}
+  </section>
+</template>
+
+<script>
+export default {
+  validate({params: {type}, redirect}) {
+    // Return false if an unacceptable type is used.
+    return true
+  },
+
+  data() {
+    return {
+
+    }
+  },
+  computed: {
+    type() { return this.$route.params.type }
+  },
+
+  created() {
+    // After confirming that the user is searched by id or phoneNumber through type parameter, the screen is rendered using the appropriate component.
+    // Now you can access this.type
+  },
+}
+</script>
